f1ac8befb80bf1c1566de479a5d7f223554e470e
budda uttra <budda10495kanw@gmail.com>
2018-12-03 17:09:09 +0700

    LAB 2

---
+++ Lab2.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        lblsalary1.Text = Val(txtsalary.Text) * 12
+        lblsalary2.Text = Val(txtsalary.Text) * 12 * 0.05
+        lblsalary3.Text = (Val(txtsalary.Text) * 12) - (Val(txtsalary.Text) * 12 * 0.05)
+    End Sub
+End Class
